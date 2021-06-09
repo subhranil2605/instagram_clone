a6ed224d023dd96885d4b8ba26d04f5758346742
Subhranil Sarkar <subhranil2605@gmail.com>
2021-06-09 22:27:48 +0530

    changed

--- libs/components/circular_avatar_image.py
+++ libs/components/circular_avatar_image.py
@@ -1,7 +1,7 @@
-from kivymd.uix.relativelayout import MDRelativeLayout
+from kivymd.uix.card import MDCard
 from kivy.properties import StringProperty
 
 
-class CircularAvatarImage(MDRelativeLayout):
+class CircularAvatarImage(MDCard):
     avatar = StringProperty()
     name = StringProperty()
--- libs/screens/home_page.py
+++ libs/screens/home_page.py
@@ -7,5 +7,5 @@ class HomePage(MDScreen):
         for i in range(10):
             self.ids.stories.add_widget(CircularAvatarImage(
                 avatar='https://images.unsplash.com/photo-1623065691913-e9a650810efd?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-                name="Subhranil Sarkar"
+                name="subhranil2605"
             ))
